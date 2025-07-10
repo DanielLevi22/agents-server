6bf621104fad9000a4fea1908205aa1be5d0ae06
Daniel Levi <daniellevi000@gmail.com>
2025-07-10 07:57:29 -0300

    feat: adiciona gemini e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,33 @@
-import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
-import { env } from "./env.ts";
-
-import { ZodTypeProvider } from "fastify-type-provider-zod"
-
-
-
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-
 app.register(fastifyCors, {
-  origin: "http://localhost:3333",
+  origin: 'http://localhost:5173',
 })
 
+app.register(fastifyMultipart)
 
-app.get("/health", (req, res) => {
-  res.send({ status: "ok" })
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+  return 'OK'
 })
 
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
-app.listen({ port: env.PORT  }, () => {
-  console.log("Server is running on port 3333")
-})
+app.listen({ port: env.PORT })
